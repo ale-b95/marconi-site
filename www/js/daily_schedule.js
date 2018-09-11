7925bda3eefc3a33d060f308e54684f90849f699
Alessandro Ballerini <alessandroballerini95@gmail.com>
2018-09-11 12:18:51 +0200

    Implementing daily schedule table

--- www/js/daily_schedule.js
+++ www/js/daily_schedule.js
@@ -5,17 +5,24 @@ $(function() {
     */
     function loadClassroomSchedule() {
         var date = new Date();
+        var n_croom = 4;
         for (var hour = 8; hour<16; hour++) {
-            $("#big_table").append(
+            $("#big-table").append(
             '<tr id="hid_'+hour+'" value="'+hour+'">'+
-            '<th>'+hour+':00</th><td></td>'+
             '</tr>');
+            $("#hid_"+hour).append('<th>'+hour+':00</th>');
+            for(var i = 0; i < n_croom; i++) {
+                $("#hid_"+hour).append('<td id=cll-"'+hour+'-'+i+'"></td>');
+            }
         }
 
+        
+
+        //for each selected classroom prints on the big table the corresponding schedule
         var pRef = firebase.database().ref('prenotation/'+date.getFullYear()+'/'+(date.getMonth() + 1)+'/'+date.getDate()+'/-LKziy2PL5xXk222mDpW/');
         pRef.once('value', snap => {
-            
             snap.forEach(childSnap => {
+                var index = 2;
                 var hour = childSnap.key;
                 var teacher_name = childSnap.val().teacher;
                 var class_name =  childSnap.val().class;
@@ -30,20 +37,53 @@ $(function() {
                     second_column = class_name + ' ' + teacher_name;
                 }
 
-                $("#hid_"+hour).empty();
-                $("#hid_"+hour).append('<th>'+hour+':00</th><td>'+ second_column +'</td>');
-                
+                $("#hid_"+hour+" td:nth-child("+index+")").text(second_column);
+
                 if (event_title) {
-                    $("#hid_"+hour).addClass('event_prenotation');
-                    $("#hid_"+hour).val(event_key);              
+                    $("#hid_"+hour+" td:nth-child(2)").addClass('event_prenotation');
+                    $("#hid_"+hour+" td:nth-child(2)").val(event_key);              
                 } else {
                     var user = firebase.auth().currentUser;
-                
                     if (user.uid == teacher_id){
-                        $("#hid_"+hour).addClass('mybook');
+                        $("#hid_"+hour+" td:nth-child(2)").addClass('mybook');
                     } else {
-                        $("#hid_"+hour).addClass('booked');
-                        $("#hid_"+hour).removeClass('clickable-row');
+                        $("#hid_"+hour+" td:nth-child(2)").addClass('booked');
+                        $("#hid_"+hour+" td:nth-child(2)").removeClass('clickable-row');
+                    }
+                }
+            });
+        });
+
+        var pRef = firebase.database().ref('prenotation/'+date.getFullYear()+'/'+(date.getMonth() + 1)+'/'+date.getDate()+'/-LM77XNN7KA037jGNulW/');
+        pRef.once('value', snap => {
+            snap.forEach(childSnap => {
+                var index = 3;
+                var hour = childSnap.key;
+                var teacher_name = childSnap.val().teacher;
+                var class_name =  childSnap.val().class;
+                var event_title = childSnap.val().event;
+                var event_key = childSnap.val().event_key;
+                var teacher_id = childSnap.val().teacher_key;
+                var second_column;
+
+                if (event_title) {
+                    second_column = event_title;
+                } else {
+                    second_column = class_name + ' ' + teacher_name;
+                }
+
+                $("#hid_"+hour+" td:nth-child("+index+")").text(second_column);
+
+                if (event_title) {
+                    $("#hid_"+hour+" td:nth-child(3)").addClass('event_prenotation');
+                    $("#hid_"+hour+" td:nth-child(3)").val(event_key);              
+                } else {
+                    var user = firebase.auth().currentUser;
+                    if (user.uid == teacher_id){
+                        $("#hid_"+hour+" td:nth-child(3)").addClass('mybook');
+                    } else {
+                        $("#hid_"+hour+" td:nth-child(3)").addClass('booked');
+                        $("#hid_"+hour+" td:nth-child(3)").removeClass('clickable-row');
                     }
                 }
             });
