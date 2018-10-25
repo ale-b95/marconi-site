93282b57b31ba3d89fbf09b60525e9792e3e3a03
Alessandro Ballerini <alessandroballerini95@gmail.com>
2018-10-25 19:03:09 +0200

    Finish

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,10 +1,22 @@
 $(function() {
   // Initialize Firebase
-  var config = {
+
+  // Test config
+  /*var config = {
     apiKey: "AIzaSyBcWoPOfCRHwFpOE2gFncQD6HtIiD1ftro",
     authDomain: "marconi-2c01d.firebaseapp.com",
     databaseURL: "https://marconi-2c01d.firebaseio.com",
     projectId: "marconi-2c01d",
+    storageBucket: "",
+    messagingSenderId: "916037440328"
+  };*/
+
+  // Release config
+  var config = {
+    apiKey: "AIzaSyBLRwoHQgSKYJYuxm9N2CCKdqv59aNbSJk ",
+    authDomain: "marconi-release.firebaseapp.com",
+    databaseURL: "https://marconi-release.firebaseio.com/",
+    projectId: "marconi-release",
     storageBucket: "",
     messagingSenderId: "916037440328"
   };
